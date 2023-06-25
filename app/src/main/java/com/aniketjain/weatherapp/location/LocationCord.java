ae1babbced36d8a5199b9c20932df9a2982098bc
VRLITHI <vrlitheesh4090@gmail.com>
2023-06-25 18:29:02 +0530

    Initial commit

---
+++ app/src/main/java/com/aniketjain/weatherapp/location/LocationCord.java
@@ -0,0 +1,9 @@
+package com.aniketjain.weatherapp.location;
+
+public class LocationCord {
+    public static String lat = "";
+    public static String lon = "";
+    //    public final static String API_KEY = "0bc14881636d2234e8c17736a470535f";
+    public final static String API_KEY = "bd5e378503939ddaee76f12ad7a97608";
+}
+
